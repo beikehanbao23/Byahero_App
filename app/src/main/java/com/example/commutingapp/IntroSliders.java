4cef9d88ee28a5f1cab7986e9d4d7e203e15387b
John Dominic <johndominicjasmin@gmail.com>
2021-07-09 23:30:02 +0800

    adding gif animations for intro sliders

--- app/src/main/java/com/example/commutingapp/IntroSliders.java
+++ app/src/main/java/com/example/commutingapp/IntroSliders.java
@@ -1,14 +1,24 @@
 package com.example.commutingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class IntroSliders extends AppCompatActivity {
+    private ViewPager2 viewPagerDisplay;
+    private LinearLayout indicatorDots;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_sliders);
+
+        viewPagerDisplay = findViewById(R.id.displayViewPager);
+        indicatorDots = findViewById(R.id.dotIndicatorLinearLayout);
+
+
     }
 }
