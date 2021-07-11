fa5b9be74c5a27bbf5a6937f05bb23d941d1142f
John Dominic <johndominicjasmin@gmail.com>
2021-07-11 21:09:17 +0800

    resizing gifs

---
+++ app/src/main/java/com/example/commutingapp/IntroSliders.java
@@ -0,0 +1,14 @@
+package com.example.commutingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroSliders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_sliders);
+    }
+}
