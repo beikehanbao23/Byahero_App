72e8742936bd180cb34bd06957678ab982f30afa
John Dominic <johndominicjasmin@gmail.com>
2021-07-22 02:16:48 +0800

    Add kotlin plugin at gradle file

---
+++ app/src/main/java/Adapter/IntroSliderAdapter.kt
@@ -0,0 +1,10 @@
+package Adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class IntroSliderAdapter {
+    inner class IntroSliderViewHolder(view: View):RecyclerView.ViewHolder(view){
+
+    }
+}
