b3fc1394363a1708345cafefa964dcc479ef3c5c
John Dominic <johndominicjasmin@gmail.com>
2021-07-22 02:19:40 +0800

    Update  gradle implementation libraries

--- app/src/main/java/com/example/commutingapp/IntroSlider.kt
+++ app/src/main/java/com/example/commutingapp/IntroSlider.kt
@@ -2,7 +2,7 @@ package com.example.commutingapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.<layout>.*
 class IntroSlider : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
