78551b445bb38b95b2c050e91874be53cc929460
John Dominic <johndominicjasmin@gmail.com>
2021-07-23 04:58:43 +0800

    Add indicator

--- app/src/main/java/com/example/commutingapp/IntroSlider.kt
+++ app/src/main/java/com/example/commutingapp/IntroSlider.kt
@@ -2,7 +2,11 @@ package com.example.commutingapp
 
 import Adapters.IntroSliderAdapter
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_intro_slider.*
 
 class IntroSlider : AppCompatActivity() {
@@ -14,4 +18,20 @@ class IntroSlider : AppCompatActivity() {
 
     }
 
+    private fun setupPageIndicators() {
+        val indicators = arrayOfNulls<ImageView>(3)
+        val layoutParameters: LinearLayout.LayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParameters.setMargins(8,0,8,0)
+
+        for(counter in indicators.indices){
+
+            indicators[counter] = ImageView(applicationContext)
+            indicators[counter].apply {
+                this?.setImageDrawable(ContextCompat.getDrawable(applicationContext,R.drawable.inactive_indicator))
+                this?.layoutParams = layoutParameters
+            }
+            linearLayout_dotsIndicator.addView(indicators[counter])
+        }
+
+    }
 }
