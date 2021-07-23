270cd219b54aea867acc2e0a8cdc01fdef7a0fca
John Dominic <johndominicjasmin@gmail.com>
2021-07-24 05:52:35 +0800

    Forgot shape indicator in v24 drawable folder(2 hours wasted fixing this problem)

--- app/src/main/java/com/example/commutingapp/IntroSlider.kt
+++ app/src/main/java/com/example/commutingapp/IntroSlider.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_intro_slider.*
 
 class IntroSlider : AppCompatActivity() {
@@ -15,44 +16,68 @@ class IntroSlider : AppCompatActivity() {
         setContentView(R.layout.activity_intro_slider)
         viewPagerSliders.adapter = IntroSliderAdapter(this)
         setupPageIndicators()
-
-
-    
-    private fun setupPageIndicators() {
-        val indicators = arrayOfNulls<ImageView>(3)
-        val layoutParameters: LinearLayout.LayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-        layoutParameters.setMargins(8,0,8,0)
-
-        for(counter in indicators.indices){
-
-            indicators[counter] = ImageView(applicationContext)
-            indicators[counter].apply {
-                this?.setImageDrawable(ContextCompat.getDrawable(applicationContext,R.drawable.inactive_indicator))
-                this?.layoutParams = layoutParameters
-            }
-            linearLayout_dotsIndicator.addView(indicators[counter])
-
-        }
-
-
-        private fun setCurrentIndicator(index:Int){
-
-        }
+        setCurrentIndicator(0)
     }
 
 
+        private fun setupPageIndicators() {
+            val indicators = arrayOfNulls<ImageView>(3)
+            val layoutParameters: LinearLayout.LayoutParams =
+                LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+
+            layoutParameters.setMargins(8, 0, 8, 0)
+            setupDisplay(indicators,layoutParameters)
 
 
+        /*
+            also - add more, extension
+            apply - use attributes of object
+            let - null checks
+            with - implicit object names
+            run - to implicit this and return type R
+
+         */
+
+        }
+
+private fun setupDisplay(indicators: Array<ImageView?>, layoutParameters: LinearLayout.LayoutParams):Unit{
+
+    for (counter in indicators.indices) {
+
+        indicators[counter] = ImageView(applicationContext)
+        indicators[counter]?.apply {
+            setInactiveIndicators(this)
+            layoutParams = layoutParameters
+        }
+        linearLayout_dotsIndicator.addView(indicators[counter])
+
+    }
+}
+    private fun setCurrentIndicator(index:Int) {
+        val counts = linearLayout_dotsIndicator.childCount
+        for (counter in 0 until counts) {
+            val imageView = linearLayout_dotsIndicator[counter] as ImageView
+            if(counter == index){
+                setActiveIndicators(imageView)
+                return
+            }
+            setInactiveIndicators(imageView)
+        }
+
+    }
+
+    private fun setActiveIndicators(imageView:ImageView){
+        imageView.setImageDrawable(ContextCompat.getDrawable(
+            applicationContext,
+            R.drawable.active_indicator))
+    }
 
 
-
-
-
-
-
-
-
-
+    private fun setInactiveIndicators(indicator:ImageView){
+    indicator.setImageDrawable(ContextCompat.getDrawable(
+        applicationContext,
+        R.drawable.ic_facebook))
+    }
 
 
 }
