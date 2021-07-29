927d623648a14f9554a946f67480cde3cb5a6e8c
John Dominic <johndominicjasmin@gmail.com>
2021-07-30 00:51:19 +0800

    Add activity for expired token

---
+++ app/src/main/java/com/example/commutingapp/TokenExpired.kt
@@ -0,0 +1,11 @@
+package com.example.commutingapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TokenExpired : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_token_expired)
+    }
+}
