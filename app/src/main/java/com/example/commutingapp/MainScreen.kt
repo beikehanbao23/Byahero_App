68ca41ec6cfd64b534aa6702086d96036f3d1c9c
John Dominic <johndominicjasmin@gmail.com>
2021-08-02 21:24:22 +0800

    Convert file from java to kotlin

---
+++ app/src/main/java/com/example/commutingapp/MainScreen.kt
@@ -0,0 +1,127 @@
+package com.example.commutingapp
+
+import FirebaseUserManager.FirebaseUserManager
+import Logger.CustomToastMessage
+import MenuButtons.BackButtonDoubleClicked
+import MenuButtons.CustomBackButton
+import MenuButtons.backButton
+import Screen.ScreenDimension
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.AccessTokenTracker
+import com.facebook.login.LoginManager
+import java.util.*
+
+class MainScreen : AppCompatActivity(), BackButtonDoubleClicked {
+    private var toastMessageBackButton: CustomToastMessage? = null
+    private var nameTextView: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ScreenDimension(window).windowToFullScreen()
+        setContentView(R.layout.activity_main_screen)
+        toastMessageBackButton =
+            CustomToastMessage(this, getString(R.string.doubleTappedMessage), 10)
+        nameTextView = findViewById(R.id.nameTextView)
+        FirebaseUserManager.initializeFirebase()
+        checkFacebookTokenIfExpired()
+        displayName()
+    }
+
+    private val name: String?
+        private get() {
+            for (userInfo in FirebaseUserManager.getFirebaseUserInstance().providerData) {
+                if (userInfo.providerId == "facebook.com") {
+                    return FirebaseUserManager.getFirebaseUserInstance().displayName
+                }
+            }
+            return getFilteredEmail(FirebaseUserManager.getFirebaseUserInstance().email)
+        }
+
+    private fun displayName() {
+        nameTextView!!.text = name
+    }
+
+    private fun getFilteredEmail(userEmail: String?): String? {
+        val emailLists = emailExtensions
+        if (userEmail != null) {
+            for (counter in emailLists.indices) {
+                val emailExtension = emailLists[counter]
+                if (userEmail.contains(emailExtension)) {
+                    return userEmail.replace(emailExtension.toRegex(), "")
+                }
+            }
+        }
+        return userEmail
+    }
+
+    private val emailExtensions: List<String>
+        private get() {
+            val list: MutableList<String> = ArrayList()
+            list.add("@gmail.com")
+            list.add("@protonmail.ch")
+            list.add("@yahoo.com")
+            list.add("@hotmail.com")
+            list.add("@outlook.com")
+            return list
+        }
+
+    private fun checkFacebookTokenIfExpired() {
+        object : AccessTokenTracker() {
+            override fun onCurrentAccessTokenChanged(
+                oldAccessToken: AccessToken,
+                currentAccessToken: AccessToken
+            ) {
+                if (currentAccessToken == null) {
+                    showExpiredTokenDialog()
+                    Log.e(javaClass.name, "Token is Expired")
+                }
+            }
+        }
+    }
+
+    fun LogoutButtonClicked(view: View?) {
+        Log.e(javaClass.name, "Logging out!")
+        putUserToLoginFlow()
+    }
+
+    private fun signOutAccount() {
+        LoginManager.getInstance().logOut()
+        FirebaseUserManager.getFirebaseAuthInstance().signOut()
+    }
+
+    private fun putUserToLoginFlow() {
+        signOutAccount()
+        showSignInForm()
+    }
+
+    private fun showSignInForm() {
+        startActivity(Intent(this, SignIn::class.java))
+        finish()
+    }
+
+    private fun showExpiredTokenDialog() {
+        startActivity(Intent(this, TokenExpired::class.java))
+        finish()
+    }
+
+    override fun onBackPressed() {
+        backButtonClicked()
+    }
+
+    override fun backButtonClicked() {
+        CustomBackButton(label@ BackButtonDoubleClicked {
+            if (backButton.isDoubleTapped()) {
+                toastMessageBackButton!!.hideToast()
+                super.onBackPressed()
+                return@BackButtonDoubleClicked
+            }
+            toastMessageBackButton!!.showToast()
+            backButton.registerFirstClick()
+        }).backButtonIsClicked()
+    }
+}
