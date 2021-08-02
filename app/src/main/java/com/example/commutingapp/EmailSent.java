875b874837e7535de5df285cc3850bd3929bf4e4
John Dominic <johndominicjasmin@gmail.com>
2021-08-02 22:21:57 +0800

    Add activity for emailsent form

---
+++ app/src/main/java/com/example/commutingapp/EmailSent.java
@@ -0,0 +1,15 @@
+package com.example.commutingapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EmailSent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.custom_emailsent_dialog);
+    }
+}
