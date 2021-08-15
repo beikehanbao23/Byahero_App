1926bdf48eff83b0f739e700ee2f9fb6cbf7bd9b
John Dominic <johndominicjasmin@gmail.com>
2021-08-15 23:49:18 +0800

    Refactor code, create object and  functions to start new activity, change class and function name

--- app/src/main/java/UI/ActivitySwitcher.kt
+++ app/src/main/java/UI/ActivitySwitcher.kt
@@ -1,4 +1,24 @@
 package UI
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+
 object ActivitySwitcher {
+    /**
+     * Start new activity and closing the previous activity, popping the previous activity at the backstack
+     */
+    fun startActivityOf(activityOfThisClass: Activity ,contextOfThisClass: Context, classToOpen:Class<*>){
+
+        contextOfThisClass.startActivity(Intent(contextOfThisClass,classToOpen::class.java))
+        activityOfThisClass.finish()
+    }
+    /**
+     * Start new activity without closing the previous activity, previous activity remains at the backstack
+     */
+
+    fun startActivityOf(contextOfThisClass: Context, classToOpen:Class<*>){
+
+        contextOfThisClass.startActivity(Intent(contextOfThisClass,classToOpen::class.java))
+    }
 }
