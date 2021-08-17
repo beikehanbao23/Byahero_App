7f0d202ff1e824ba05096ecbab81426bb3cf9788
John Dominic <johndominicjasmin@gmail.com>
2021-08-17 14:40:04 +0800

    Code refactoring

--- app/src/main/java/MenuButtons/CustomBackButton.java
+++ app/src/main/java/MenuButtons/CustomBackButton.java
@@ -3,59 +3,43 @@ package MenuButtons;
 import android.app.Activity;
 import android.content.Context;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.rejowan.cutetoast.CuteToast;
 
 import Logger.CustomToastMessage;
 
-public class BackButton extends AppCompatActivity {
+public class CustomBackButton  {
 
     private static final int timeDelayInMillis = 1800;
     private static long backPressedTime = 0;
     private final Activity activity;
-    private final Context context;
     private final CustomToastMessage toastMessage;
 
-    public BackButton(Activity activity, Context context) {
+    public CustomBackButton(Activity activity, Context context) {
         this.activity = activity;
-        this.context = context;
         toastMessage = new CustomToastMessage(context, "Tap again to exit", CuteToast.NORMAL);
     }
 
 
-    /*
-
-        fun test(activity: Activity,context: Context) {
-            val toastMessageBackButton = CustomToastMessage(context, "Tap again to exit!", 10)
-            CustomBackButton(label@BackButtonOnPressed {
-                if (backButton.isDoubleTapped()) {
-                    toastMessageBackButton.hideToast()
-
-                    activity.finish()
-                    return@BackButtonOnPressed
-                }
-                toastMessageBackButton.showToast()
-                backButton.registerFirstClick()
-            }).backButtonIsClicked()
-        }
-*/
 
 
-
-    public void doubleClickOnPressed() {
-        if (isDoubleTapped()) {
-            toastMessage.hideToast();
-            activity.finish();
+    public void applyDoubleClickToExit() {
+        if (doubleClicked()) {
+            terminateActivity();
             return;
         }
+        beginToastMessage();
+    }
+    private static boolean doubleClicked() {
+
+        return backPressedTime + timeDelayInMillis > System.currentTimeMillis();
+    }
+    private void terminateActivity(){
+        toastMessage.hideToast();
+        activity.finish();
+    }
+    private void beginToastMessage(){
         toastMessage.showToast();
         registerFirstClick();
-    }
-    
-    private static boolean isDoubleTapped() {
-        
-        return backPressedTime + timeDelayInMillis > System.currentTimeMillis();
     }
     private static void registerFirstClick() {
         
