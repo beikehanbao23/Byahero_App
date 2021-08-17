396fd0dbaceb4ac8e52b31ef4bc327eefd3191fc
John Dominic <johndominicjasmin@gmail.com>
2021-08-17 08:02:44 +0800

    Add more null safety, but it works now

--- app/src/main/java/Users/UserManager.kt
+++ app/src/main/java/Users/UserManager.kt
@@ -11,16 +11,17 @@ class UserManager constructor(
 ) : User(context, email, password, confirmPassword) {
 
 
-
-
+    /**
+     * kind of weird putting '== true' there, but it's part of kotlin null safety btw.
+     */
     fun signUpValidationFail(): Boolean {
-        return super.validateEmailFail() ||
-                super.validatePasswordFail() ||
+        return super.validateEmailFail() == true ||
+                super.validatePasswordFail() == true||
                 super.validateConfirmPasswordFail() == true
     }
 
     fun signInValidationFail(): Boolean {
-        return super.validateEmailFail() ||
-                super.validatePasswordFail()
+        return super.validateEmailFail() == true ||
+                super.validatePasswordFail() == true
     }
 }
