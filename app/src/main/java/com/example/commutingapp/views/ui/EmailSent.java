da102863afd79c20c255cdbea5d839f30a01f93f
John Dominic <johndominicjasmin@gmail.com>
2021-08-26 16:42:41 +0800

    Fix bug, showing multiple Main screen, applying mvvm architecture concept

--- app/src/main/java/com/example/commutingapp/views/ui/EmailSent.java
+++ app/src/main/java/com/example/commutingapp/views/ui/EmailSent.java
@@ -5,9 +5,11 @@ import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.commutingapp.R;
 import com.example.commutingapp.databinding.*;
+import com.example.commutingapp.viewmodels.EmailSentViewModel;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseNetworkException;
 import java.util.Objects;
@@ -22,18 +24,19 @@ import static com.example.commutingapp.R.string.*;
 
 public class EmailSent extends AppCompatActivity implements BindingDestroyer, AttributesInitializer {
 
-    private final long twoMinutes = 120000;
+
     private final long threadInterval = 2000;
     private DialogPresenter customPopupDialog;
-    private final long DELAY_INTERVAL = 1500;
+    private final long DELAY_INTERVAL_FOR_NO_INTERNET_DIALOG = 1500;
+    private final long DELAY_INTERVAL_FOR_MAIN_SCREEN_DIALOG = 2950;
     private CountDownTimer verificationTimer;
     private CustomToastMessage toastMessageBackButton;
     private volatile boolean exitThread;
     private CustomEmailsentDialogBinding emailDialogBinding;
     private CircularProgressbarBinding progressbarBinding;
+    private EmailSentViewModel viewModel;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         initializeAttributes();
@@ -42,51 +45,49 @@ public class EmailSent extends AppCompatActivity implements BindingDestroyer, At
 
 
     }
-
-    @Override
-    public void initializeAttributes() {
+    @Override public void initializeAttributes() {
         emailDialogBinding = CustomEmailsentDialogBinding.inflate(getLayoutInflater());
         progressbarBinding = CircularProgressbarBinding.bind(emailDialogBinding.getRoot());
         setContentView(emailDialogBinding.getRoot());
-
         customPopupDialog = new CustomDialogs(this);
         toastMessageBackButton = new CustomToastMessage(this, getString(doubleTappedMessage), 10);
-    }
+        viewModel = new ViewModelProvider(this).get(EmailSentViewModel.class);
 
-    @Override
-    public void destroyBinding() {
+    }
+    @Override public void destroyBinding() {
         emailDialogBinding = null;
         progressbarBinding = null;
     }
-    private void refreshEmailAutomatically() throws Exception {
+
+
+    private void refreshEmailAutomatically() {
         new Thread(() -> {
             while (!exitThread) {
                 reloadUserEmail();
-                Log.e("THREAD STATUS: ","RUNNING");
+                Log.d("THREAD STATUS: ","RUNNING");
                 try { Thread.sleep(threadInterval); } catch (InterruptedException e) { e.printStackTrace(); }
             }
         }).start();
-
-
     }
 
 
-    @Override
-    protected void onStart() {
+    @Override protected void onStop() {
+        super.onStop();
+        exitThread = true;
+
+    }
+    @Override protected void onStart() {
         super.onStart();
-
-            setThreadDisabled(false);
-            try { refreshEmailAutomatically(); } catch (Exception exception) { exception.printStackTrace(); }
+        exitThread = false;
+        refreshEmailAutomatically();
     }
 
-    private void setThreadDisabled(Boolean threadState){
-     exitThread = threadState;
-    }
 
     private void reloadUserEmail() {
 
             FirebaseManager.getFirebaseUserInstance().reload().addOnCompleteListener(emailReload -> {
                 if (emailReload.isSuccessful() && FirebaseManager.getFirebaseUserInstance().isEmailVerified()) {
+                    exitThread = true;
                     showMainScreenActivity();
                     return;
                 }
@@ -103,7 +104,7 @@ public class EmailSent extends AppCompatActivity implements BindingDestroyer, At
         try {
             throw Objects.requireNonNull(task.getException());
         }catch (FirebaseNetworkException firebaseNetworkException) {
-            setThreadDisabled(true);
+            exitThread = true;
             showNoInternetActivity();
         }catch (Exception ignored){}
     }
@@ -114,29 +115,32 @@ public class EmailSent extends AppCompatActivity implements BindingDestroyer, At
         new Handler().postDelayed(()-> {
            ActivitySwitcher.INSTANCE.startActivityOf(this, NoInternet.class);
            progressbarBinding.circularProgressBar.setVisibility(View.INVISIBLE);
-       },DELAY_INTERVAL);
+       }, DELAY_INTERVAL_FOR_NO_INTERNET_DIALOG);
     }
 
+    private void showMainScreenActivity() {
+        progressbarBinding.circularProgressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(()->{
+            ActivitySwitcher.INSTANCE.startActivityOf(this,this, MainScreen.class);
+            progressbarBinding.circularProgressBar.setVisibility(View.INVISIBLE);
+        },DELAY_INTERVAL_FOR_MAIN_SCREEN_DIALOG);
 
+    }
 
     @Override
     public void onBackPressed() {
        new CustomBackButton(this,this).applyDoubleClickToExit();
     }
 
-
-
     private void displayUserEmailToTextView() {
-        String userEmail = FirebaseManager.getFirebaseUserInstance().getEmail();
-        if (userEmail != null) {
+        viewModel.displayUserEmailToTextView().observe(this,userEmail->{
             emailDialogBinding.textViewEmail.setText(userEmail);
-        }
+        });
     }
-
 
     public void resendEmailIsClicked(View view) {
         FirebaseManager.getFirebaseUserInstance().sendEmailVerification().addOnCompleteListener(task -> {
-            startTimerForVerification();
+            startVerificationTimer();
             if (task.isSuccessful()) {
                 customPopupDialog.showSuccessDialog("New email sent", getString(resendEmailSuccessMessage));
                 return;
@@ -157,41 +161,21 @@ public class EmailSent extends AppCompatActivity implements BindingDestroyer, At
         emailDialogBinding.ResendVerificationButton.setEnabled(true);
     }
 
-    private void startTimerForVerification() {
-
-        verificationTimer = new CountDownTimer(twoMinutes, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                long secondsLeft = millisUntilFinished / 1000;
-                displayWhenVerificationTimerStarted(secondsLeft);
-            }
-
-            @Override
-            public void onFinish() {
+    private void startVerificationTimer() {
+        viewModel.startTimer();
+        viewModel.timerOnRunning().observe(this, this::displayWhenVerificationTimerStarted);
+        viewModel.timerOnFinished().observe(this,timer->{
+            if(timer.getContentIfNotHandled()!=null) {
                 displayWhenVerificationTimerIsFinished();
             }
-        }.start();
-
-    }
-
-    private void removeVerificationTimer() {
-        if (verificationTimer != null) {
-            verificationTimer.cancel();
-        }
-
-    }
-
-    private void showMainScreenActivity() {
-
-        ActivitySwitcher.INSTANCE.startActivityOf(this,this, MainScreen.class);
+        });
     }
 
     @Override
     protected void onDestroy() {
-        removeVerificationTimer();
-        setThreadDisabled(true);
-        destroyBinding();
+        exitThread = true;
         super.onDestroy();
+        destroyBinding();
     }
 
 
