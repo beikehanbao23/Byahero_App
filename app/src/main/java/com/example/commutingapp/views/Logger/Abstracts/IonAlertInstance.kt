ee30e485648a473ea26f70cde36cb6216fb36892
John Dominic <johndominicjasmin@gmail.com>
2021-09-04 21:48:22 +0800

    Add internal class for IonAlert 3rd party library class

--- app/src/main/java/com/example/commutingapp/views/Logger/Abstracts/IonAlertInstance.kt
+++ app/src/main/java/com/example/commutingapp/views/Logger/Abstracts/IonAlertInstance.kt
@@ -1,4 +1,15 @@
 package com.example.commutingapp.views.Logger.Abstracts
 
-class IonAlertInstance {
+import android.content.Context
+import id.ionbit.ionalert.IonAlert
+
+internal class IonAlertInstance(var context: Context) {
+     fun dialogIonAlert(title: String, contextText: String, type: Int): IonAlert {
+        return IonAlert(context, type).apply {
+            titleText = title
+            contentText = contextText
+        }
+    }
+
+
 }
