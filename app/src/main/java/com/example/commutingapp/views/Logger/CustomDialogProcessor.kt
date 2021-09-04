d93af5560bfefe0027c8a4062dd7cb14f2fc07c1
John Dominic <johndominicjasmin@gmail.com>
2021-09-04 22:06:34 +0800

    Change function names, add dependency for noInternetDialog wrapper and FixDialog

--- app/src/main/java/com/example/commutingapp/views/Logger/CustomDialogProcessor.kt
+++ app/src/main/java/com/example/commutingapp/views/Logger/CustomDialogProcessor.kt
@@ -1,15 +1,37 @@
 package com.example.commutingapp.views.Logger
 
+import android.app.Dialog
 import android.content.Context
-import com.example.commutingapp.views.Logger.Abstracts.SuccessDialogWrapper
+import com.example.commutingapp.views.Logger.Abstracts.ErrorStateDialogWrapper
+import com.example.commutingapp.views.Logger.Abstracts.NoInternetDialogWrapper
+import com.example.commutingapp.views.Logger.Abstracts.SuccessStateDialogWrapper
+import com.example.commutingapp.views.Logger.Abstracts.WarningStateDialogWrapper
 
 class CustomDialogProcessor(var context:Context) {
-
+    private val noInternetDialogWrapper = NoInternetDialogWrapper(context)
+    private val noInternetDialog = FixDialogManager(noInternetDialogWrapper)
 
     fun showSuccessDialog(title:String,contentText:String) {
-       CustomDialogManager(SuccessDialogWrapper(context)).apply{
+       StateDialogManager(SuccessStateDialogWrapper(context)).apply{
            showDialog(title,contentText)
        }
     }
 
+    fun showErrorDialog(title:String,contentText: String){
+        StateDialogManager(ErrorStateDialogWrapper(context)).apply{
+            showDialog(title,contentText)
+        }
+    }
+
+    fun showWarningDialog(title:String,contentText: String){
+        StateDialogManager(WarningStateDialogWrapper(context)).apply{
+            showDialog(title,contentText)
+        }
+    }
+
+    fun showNoInternetDialog(){
+        noInternetDialog.showDialog()
+    }
+
+    fun noInternetDialogCallback(): Dialog = noInternetDialogWrapper.dialog
 }
