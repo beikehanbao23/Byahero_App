473ca2fc9a5a2927dddf7727deba4217213e8d36
John Dominic <johndominicjasmin@gmail.com>
2021-09-09 19:55:06 +0800

    Add UI test

--- app/src/androidTest/java/com/example/commutingapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/commutingapp/ExampleInstrumentedTest.java
@@ -17,11 +17,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.commutingapp", appContext.getPackageName());
+
     }
 
 }
