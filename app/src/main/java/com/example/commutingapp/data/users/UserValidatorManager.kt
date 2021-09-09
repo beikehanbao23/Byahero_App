e557aba9c44cbe7f7b9455b59006ac8f80d4c4e5
John Dominic <johndominicjasmin@gmail.com>
2021-09-09 20:13:50 +0800

    Refactor codes

--- app/src/main/java/com/example/commutingapp/data/users/UserValidatorManager.kt
+++ app/src/main/java/com/example/commutingapp/data/users/UserValidatorManager.kt
@@ -1,26 +1,18 @@
 package com.example.commutingapp.data.users
 
-import android.content.Context
-import android.widget.EditText
-
-class UserValidatorManager constructor(
-    context: Context,
-    email: EditText,
-    password: EditText,
-    confirmPassword: EditText?
-) : UserValidatorModel(context, email, password, confirmPassword) {
+class UserValidatorManager constructor(var validator: UserValidator) {
 
 
     /**kind of weird putting '== true' there, but it's part of kotlin null safety btw.*/
     fun signUpValidationFail(): Boolean {
-        return super.validationEmailFailed() == true ||
-                super.validationPasswordFailed() == true||
-                super.validationConfirmPasswordFailed() == true
+        return validator.validationEmailFailed() == true ||
+                validator.validationPasswordFailed() == true||
+                validator.validationConfirmPasswordFailed() == true
     }
 
     fun signInValidationFail(): Boolean {
-        return super.validationEmailFailed() == true ||
-                super.validationPasswordFailed() == true
+        return validator.validationEmailFailed() == true ||
+                validator.validationPasswordFailed() == true
     }
 
 }
