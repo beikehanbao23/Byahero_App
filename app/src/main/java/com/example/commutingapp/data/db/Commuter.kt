29edc8a9acc5fb0c3033e94b94973fb674ec3fcb
John Dominic <johndominicjasmin@gmail.com>
2021-09-12 23:18:27 +0800

    Add 'WentPlaces ' parameter in constructor

--- app/src/main/java/com/example/commutingapp/data/db/Commuter.kt
+++ app/src/main/java/com/example/commutingapp/data/db/Commuter.kt
@@ -5,13 +5,13 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Commute_Table")
-data class Run(
+data class Commuter(
     var image: Bitmap? = null,
     var timestamp: Long = 0L,
     var averageSpeed_KmH: Float = 0f,
     var distanceInMeters:Int = 0,
-    var timeSpent: Long = 0L
-) {
+    var timeInMillis: Long = 0L,
+    var wentPlaces:String? = null) {
 
 @PrimaryKey(autoGenerate = true)
 var ID: Int? = null
