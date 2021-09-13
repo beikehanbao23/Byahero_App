6f1bf68cf718801fd1e9ab6ee63fc1896ae91016
John Dominic <johndominicjasmin@gmail.com>
2021-09-13 11:53:05 +0800

    Add Database

---
+++ app/src/main/java/com/example/commutingapp/data/db/CommuterDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.commutingapp.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Commuter::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class CommuterDatabase:RoomDatabase(){
+
+    abstract fun getRunDao():CommuterDao
+}
