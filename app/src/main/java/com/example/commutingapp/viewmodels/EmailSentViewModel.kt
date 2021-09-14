a01d6b0d89a140614b36bdab0981cc7eb4efc501
John Dominic <johndominicjasmin@gmail.com>
2021-09-14 21:38:02 +0800

    Move package

--- app/src/main/java/com/example/commutingapp/viewmodels/EmailSentViewModel.kt
+++ app/src/main/java/com/example/commutingapp/viewmodels/EmailSentViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.commutingapp.utils.FirebaseUserManager.AuthenticationManager
+import com.example.commutingapp.data.Authenticate.AuthenticationManager
 import com.example.commutingapp.utils.ui_utilities.Event
 import com.google.firebase.FirebaseNetworkException
 import kotlinx.coroutines.*
