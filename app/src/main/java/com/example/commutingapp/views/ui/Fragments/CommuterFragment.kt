b2c35e1ca5ffe8167110b6479b1f9da3f16edbc5
John Dominic <johndominicjasmin@gmail.com>
2021-09-17 20:42:07 +0800

    Add Class for Fragments

---
+++ app/src/main/java/com/example/commutingapp/views/ui/Fragments/CommuterFragment.kt
@@ -0,0 +1,7 @@
+package com.example.commutingapp.views.ui.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.commutingapp.R
+
+class CommuterFragment: Fragment(R.layout.commuter_fragment){
+}
---
+++ app/src/main/java/com/example/commutingapp/views/ui/Fragments/StatisticsFragment.kt
@@ -0,0 +1,4 @@
+package com.example.commutingapp.views.ui.Fragments
+
+class StatisticsFragment {
+}
