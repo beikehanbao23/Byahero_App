324170ddb9a9d9e73d9a4de10d3fef20ee3dea5f
John Dominic <johndominicjasmin@gmail.com>
2021-09-17 20:56:31 +0800

    Add ViewModels

---
+++ app/src/main/java/com/example/commutingapp/viewmodels/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.commutingapp.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.commutingapp.data.repositories.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(repository: MainRepository):ViewModel(){
+}
---
+++ app/src/main/java/com/example/commutingapp/viewmodels/StatisticsViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.commutingapp.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.commutingapp.data.repositories.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StatisticsViewModel @Inject constructor(repository: MainRepository) : ViewModel()
