3ea57ada7a669bd9bd588c10d9016fec6bb6f000
John Dominic <johndominicjasmin@gmail.com>
2021-09-18 23:58:37 +0800

    Add Fragment class

--- app/src/main/java/com/example/commutingapp/views/ui/Fragments/CommuterFragment.kt
+++ app/src/main/java/com/example/commutingapp/views/ui/Fragments/CommuterFragment.kt
@@ -1,7 +1,13 @@
 package com.example.commutingapp.views.ui.Fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.commutingapp.R
 
+import com.example.commutingapp.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class CommuterFragment: Fragment(R.layout.commuter_fragment){
+private val viewModel : MainViewModel by viewModels()
 }
