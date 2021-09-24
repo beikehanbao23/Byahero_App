fc2a5fde2e4634345630e0d2d9d37f3fcde6aae0
John Dominic <johndominicjasmin@gmail.com>
2021-09-25 01:46:50 +0800

    Add WeatherViewModel.kt class && add statistics viewModel in StatisticsFragment.kt

--- app/src/main/java/com/example/commutingapp/views/ui/Fragments/SetupFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.commutingapp.views.ui.Fragments
-
-import androidx.fragment.app.Fragment
-import com.example.commutingapp.R
-
-class SetupFragment:Fragment(R.layout.setup_fragment) {
-}
--- app/src/main/java/com/example/commutingapp/views/ui/Fragments/StatisticsFragment.kt
+++ app/src/main/java/com/example/commutingapp/views/ui/Fragments/StatisticsFragment.kt
@@ -3,10 +3,10 @@ package com.example.commutingapp.views.ui.Fragments
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.commutingapp.R
-import com.example.commutingapp.viewmodels.MainViewModel
+import com.example.commutingapp.viewmodels.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StatisticsFragment: Fragment(R.layout.statistics_fragment)  {
-    private val viewModel : MainViewModel by viewModels()
+    private val viewModel : StatisticsViewModel by viewModels()
 }
