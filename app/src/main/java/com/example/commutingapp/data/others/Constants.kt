6eb1bef20c05f361c073e1d140821d8f444e6a97
John Dominic <johndominicjasmin@gmail.com>
2021-09-27 23:24:55 +0800

    Add constants for service

--- app/src/main/java/com/example/commutingapp/data/others/Constants.kt
+++ app/src/main/java/com/example/commutingapp/data/others/Constants.kt
@@ -1,7 +1,13 @@
 package com.example.commutingapp.data.others
 
 object Constants {
-const val COMMUTER_DATABASE_FILE_NAME = "Commute_DB"
+    const val COMMUTER_DATABASE_FILE_NAME = "Commute_DB"
 
-    const val REQUEST_CODE_LOCATION_PERMISSION = 1;
+    const val REQUEST_CODE_LOCATION_PERMISSION = 1
+
+    const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
+    const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
+    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+
+
 }
