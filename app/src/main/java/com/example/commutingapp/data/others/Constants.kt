10ac818cb7e35de50bac67758ae648f4eed7fa9d
johndominic <johndominicjasmin@gmail.com>
2021-09-29 23:53:11 +0800

    Add intervals and failure message

--- app/src/main/java/com/example/commutingapp/data/others/Constants.kt
+++ app/src/main/java/com/example/commutingapp/data/others/Constants.kt
@@ -13,5 +13,8 @@ object Constants {
     const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
     const val NOTIFICATION_CHANNEL_NAME = "Tracking"
     const val NOTIFICATION_ID = 1
+    const val FACEBOOK_CONNECTION_FAILURE = "CONNECTION_FAILURE: CONNECTION_FAILURE"
 
+    const val FASTEST_LOCATION_UPDATE_INTERVAL = 5000L
+    const val NORMAL_LOCATION_UPDATE_INTERVAL = 10000L
 }
