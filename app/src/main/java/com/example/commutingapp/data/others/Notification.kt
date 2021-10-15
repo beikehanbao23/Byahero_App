505e3626d809ef488359e738c881845d38c71908
johndominic <johndominicjasmin@gmail.com>
2021-10-15 20:42:07 +0800

    Create interface

---
+++ app/src/main/java/com/example/commutingapp/data/others/Notification.kt
@@ -0,0 +1,7 @@
+package com.example.commutingapp.data.others
+
+interface Notification {
+    fun createNotification()
+    fun updateNotification()
+    fun createNotificationChannel()
+}
---
+++ app/src/main/java/com/example/commutingapp/data/others/Watch.kt
@@ -0,0 +1,7 @@
+package com.example.commutingapp.data.others
+
+interface Watch {
+    fun pause()
+    fun stop()
+    fun start()
+}
