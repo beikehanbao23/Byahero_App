10b16ba3517a63bf792cd996d099c6a94bf5f1f7
John_Dominic <johndominicjasmin@gmail.com>
2021-12-25 00:56:35 +0800

    Add data class model for recyclerview

---
+++ app/src/main/java/com/example/commutingapp/views/ui/recycler_view/WeatherRecyclerViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.commutingapp.views.ui.recycler_view
+
+data class WeatherRecyclerViewModel(
+    val time:String,
+    val icon:String,
+    val temperature:String,
+    val windSpeed:String
+)
