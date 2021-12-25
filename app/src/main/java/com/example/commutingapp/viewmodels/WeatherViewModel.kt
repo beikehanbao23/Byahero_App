a74e187c17b6bb6902ec84148d3750c433d0af04
John_Dominic <johndominicjasmin@gmail.com>
2021-12-26 01:22:02 +0800

    Render data at recyclerview, from Weather API

--- app/src/main/java/com/example/commutingapp/viewmodels/WeatherViewModel.kt
+++ app/src/main/java/com/example/commutingapp/viewmodels/WeatherViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.commutingapp.viewmodels
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Geocoder
+import android.location.LocationManager
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.commutingapp.BuildConfig
 import com.example.commutingapp.data.api.Weather
 import com.example.commutingapp.data.api.WeatherService
 import com.example.commutingapp.data.api.WeatherServiceAPI
@@ -10,18 +15,22 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import timber.log.Timber
+import java.io.IOException
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class WeatherViewModel : ViewModel() {
     private var map: HashMap<String, String> = HashMap()
     private var request: WeatherServiceAPI
-    private val location = MutableLiveData<String>()
-    fun getLocation():LiveData<String> = location
+    private val userLocation = MutableLiveData<String>()
+    private val searchedLocation = MutableLiveData<Weather>()
+    fun getSearchedLocation(): LiveData<Weather> = searchedLocation
+    fun getCurrentLocation(): LiveData<String> = userLocation
 
     init {
         map.apply {
-            this["key"] = "9b4e21f5ba214091bd7110616212312"
-            this["q"] = "Manila"
+            this["key"] = BuildConfig.WEATHER_API_KEY
             this["days"] = "1"
             this["aqi"] = "yes"
             this["alerts"] = "yes"
@@ -35,15 +44,36 @@ class WeatherViewModel : ViewModel() {
         call.enqueue(weatherCallback())
     }
 
+    @SuppressLint("MissingPermission")
+    fun getLastKnownLocation(context: Context) {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        val geocoder = Geocoder(context, Locale.getDefault())
+        try {
+            location?.let {
+                val addresses = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+                addresses.forEach { address ->
+                    val city = "${address.locality} ${address.thoroughfare}"
+                    userLocation.value = city
+                }
+            }
+        } catch (e: IOException) {
+            Timber.e("Weather View Model: ${e.message}")
+        }
+    }
+
     private fun weatherCallback() = object : Callback<Weather> {
         override fun onResponse(call: Call<Weather>, response: Response<Weather>) {
-            val result = response.body()
-            location.value = result!!.location.name
 
+            if(!response.isSuccessful){
+                Timber.e("Response no success: ${response.code()}")
+                return
+            }
+             searchedLocation.value = response.body()
         }
 
         override fun onFailure(call: Call<Weather>, t: Throwable) {
-            Timber.e("Weather on failure ${t.message}")
+            Timber.e("Weather View Model on failure ${t.message}")
         }
     }
 }
--- app/src/main/java/com/example/commutingapp/views/ui/fragments/WeatherFragment.kt
+++ app/src/main/java/com/example/commutingapp/views/ui/fragments/WeatherFragment.kt
@@ -1,24 +1,55 @@
 package com.example.commutingapp.views.ui.fragments
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.commutingapp.R
 import com.example.commutingapp.databinding.WeatherFragmentBinding
+import com.example.commutingapp.utils.InternetConnection.Connection
+import com.example.commutingapp.utils.others.Constants.KEY_USER_CITY_LOCATION_SHARED_PREFERENCE
+import com.example.commutingapp.utils.others.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.example.commutingapp.utils.others.TrackingPermissionUtility.hasLocationPermission
+import com.example.commutingapp.utils.others.TrackingPermissionUtility.requestLocationPermission
 import com.example.commutingapp.viewmodels.WeatherViewModel
 import com.example.commutingapp.views.adapters.WeatherRecyclerViewAdapter
+import com.example.commutingapp.views.dialogs.DialogDirector
 import com.example.commutingapp.views.ui.recycler_view.WeatherRecyclerViewModel
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import timber.log.Timber
+
+
+
 
 @AndroidEntryPoint
-class WeatherFragment: Fragment(R.layout.weather_fragment){
+class WeatherFragment: Fragment(R.layout.weather_fragment), EasyPermissions.PermissionCallbacks{
     private var binding: WeatherFragmentBinding? = null
-    private lateinit var listOfWeatherModel:List<WeatherRecyclerViewModel>
-    private lateinit var weatherViewModel:WeatherViewModel
+    private var listOfWeatherModel:MutableList<WeatherRecyclerViewModel> = ArrayList()
+    private lateinit var weatherViewModel: WeatherViewModel
+    private  var ui: HashMap<String, SharedPreferences> = HashMap()
+    private lateinit var userCityLocation :SharedPreferences
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        userCityLocation = context.getSharedPreferences(KEY_USER_CITY_LOCATION_SHARED_PREFERENCE,Context.MODE_PRIVATE)
 
+        /*
+        ui = HashMap()
+        ui["userCity"] = context.getSharedPreferences(KEY_USER_CITY_LOCATION_SHARED_PREFERENCE,Context.MODE_PRIVATE)
+        ui["temperature"] = context.getSharedPreferences(KEY_USER_CITY_LOCATION_TEMPERATURE,Context.MODE_PRIVATE)
+        ui["weatherCondition"] = context.getSharedPreferences(KEY_USER_CITY_WEATHER_CONDITION_TEXT,Context.MODE_PRIVATE)
+        ui["weatherIcon"] = context.getSharedPreferences(KEY_USER_CITY_WEATHER_CONDITION_ICON,Context.MODE_PRIVATE)
+
+
+         */
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = WeatherFragmentBinding.inflate(inflater,container,false)
@@ -28,19 +59,76 @@ class WeatherFragment: Fragment(R.layout.weather_fragment){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-            listOfWeatherModel = ArrayList()
-            binding!!.recyclerViewDisplay.adapter = WeatherRecyclerViewAdapter(requireActivity(),listOfWeatherModel)
-            weatherViewModel = WeatherViewModel()
-            weatherViewModel.requestWeather("Batangas")
+        initializeAttributes()
+        provideObservers()
 
+        if(!Connection.hasInternetConnection(requireContext())){
+             DialogDirector(requireActivity()).buildNoInternetDialog()
+            binding!!.circularProgressBar.visibility = View.INVISIBLE
+            return
+        }
 
-            weatherViewModel.getLocation().observe(viewLifecycleOwner){
-                Toast.makeText(requireContext(),"Location is: $it",Toast.LENGTH_SHORT).show()
+        if(hasLocationPermission(requireContext())){
+            getUserCityLocation()
+        }else{
+            requestLocationPermission(this)
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n", "NotifyDataSetChanged")
+    private fun provideObservers(){
+        weatherViewModel.getCurrentLocation().observe(viewLifecycleOwner){
+            updateUserCityNameToSharedPreference(it)
+        }
+
+        weatherViewModel.getSearchedLocation().observe(viewLifecycleOwner){weatherInfo->
+            binding!!.textViewCityName.text = "${weatherInfo.location.region}, ${weatherInfo.location.country}"
+            binding!!.textViewTemperature.text = "${weatherInfo.current.temp_c}°c"
+            binding!!.textViewWeatherCondition.text = weatherInfo.current.condition.text
+            Picasso.Builder(activity).build().load("http:"+(weatherInfo.current.condition.icon) ).into(binding!!.imageViewWeatherCondition)
+            weatherInfo.forecast.forecastday[0].hour.forEach{
+                listOfWeatherModel.add(WeatherRecyclerViewModel(
+                    time = it.time,
+                    icon = it.condition.icon,
+                    temperature = it.temp_c.toString(),
+                    windSpeed = it.wind_kph.toString()
+                ))
             }
+            binding!!.recyclerViewDisplay.adapter?.notifyDataSetChanged()
+            binding!!.circularProgressBar.visibility = View.INVISIBLE
+        }
+    }
 
 
 
+    private fun getUserCityLocation(){
+        weatherViewModel.getLastKnownLocation(requireContext())
+       val currentCity = getUserCityNameFromSharedPreference()
+        currentCity?.let(weatherViewModel::requestWeather)
+        binding!!.circularProgressBar.visibility = View.VISIBLE
+    }
 
+    private fun updateUserCityNameToSharedPreference(cityName:String){
+        userCityLocation.edit().putString(KEY_USER_CITY_LOCATION_SHARED_PREFERENCE,cityName).apply()
+    }
+    private fun getUserCityNameFromSharedPreference():String? = userCityLocation.getString(KEY_USER_CITY_LOCATION_SHARED_PREFERENCE,"")
+    private fun initializeAttributes(){
+        binding!!.recyclerViewDisplay.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        binding!!.recyclerViewDisplay.adapter = WeatherRecyclerViewAdapter(requireActivity(),listOfWeatherModel)
+        weatherViewModel = WeatherViewModel()
+    }
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        if(requestCode == REQUEST_CODE_LOCATION_PERMISSION){
+            getUserCityLocation()
+            return
+        }
+    }
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        Timber.e("onPermissionsDenied requestCode is $requestCode")
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
     }
 
 
