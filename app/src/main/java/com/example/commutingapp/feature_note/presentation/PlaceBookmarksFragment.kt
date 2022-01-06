e4c51e5e6c85bac5498f869afa5868df1f328d17
John_Dominic <johndominicjasmin@gmail.com>
2022-01-06 21:21:42 +0800

    Setup place recycler view

--- app/src/main/java/com/example/commutingapp/feature_note/presentation/PlaceBookmarksFragment.kt
+++ app/src/main/java/com/example/commutingapp/feature_note/presentation/PlaceBookmarksFragment.kt
@@ -1,31 +1,64 @@
 package com.example.commutingapp.feature_note.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.commutingapp.R
-import com.example.commutingapp.databinding.FragmentListBinding
-import com.example.commutingapp.feature_note.presentation.place.components.PlaceBookmarksViewModel
+import com.example.commutingapp.databinding.PlaceBookmarksFragmentBinding
+import com.example.commutingapp.views.adapters.PlaceBookmarksAdapter
+import com.example.commutingapp.views.ui.recycler_view_model.PlaceBookmarksRVModel
+import com.mapbox.mapboxsdk.geometry.LatLng
 
 
 class PlaceBookmarksFragment : Fragment(R.layout.place_bookmarks_fragment) {
-    private lateinit var binding:FragmentListBinding
-    private val bookmarksViewModel:PlaceBookmarksViewModel by viewModels()
+    private var binding:PlaceBookmarksFragmentBinding? = null
+    private var listOfPlaceBookmarks:MutableList<PlaceBookmarksRVModel> = ArrayList()
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = PlaceBookmarksFragmentBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    private fun setupRecyclerView() = binding!!.recyclerViewDisplay.apply {
+        layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        adapter?.setHasStableIds(true)
+        adapter = PlaceBookmarksAdapter(requireActivity(), listOfPlaceBookmarks )
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonNavigate.setOnClickListener {
+        fillList()
+        setupRecyclerView()
+        binding!!.buttonAdd.setOnClickListener {
             val action = PlaceBookmarksFragmentDirections.actionListFragmentToCommuterFragment(isOpenFromBookmarks = true)
-            Navigation.findNavController(binding.root).navigate(action)
+            Navigation.findNavController(binding!!.root).navigate(action)
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
-        binding = FragmentListBinding.inflate(inflater, container, false)
-        return binding.root
+    @SuppressLint("NotifyDataSetChanged")
+    private fun fillList(){
+        for(i in 1..12){
+
+            listOfPlaceBookmarks.add(
+                PlaceBookmarksRVModel(
+                    placeName = "Dita Sta Rosa",
+                    placeText = "Agatha homes phase 1",
+                    location = LatLng(14.5995,120.9842)
+                )
+            )
+        }
+        binding!!.recyclerViewDisplay.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
--- app/src/main/java/com/example/commutingapp/views/adapters/PlaceBookmarksAdapter.kt
+++ app/src/main/java/com/example/commutingapp/views/adapters/PlaceBookmarksAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.commutingapp.views.adapters
 
-class PlaceBookmarksAdapter {
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.commutingapp.databinding.PlaceBookmarksRecyclerviewAdapterBinding
+import com.example.commutingapp.views.ui.recycler_view_model.PlaceBookmarksRVModel
+
+class PlaceBookmarksAdapter(
+    private val activity: Activity,
+    private val listOfPlaceBookmarks : List<PlaceBookmarksRVModel>
+): RecyclerView.Adapter<PlaceBookmarksAdapter.ViewHolder>() {
+
+    private lateinit var binding: PlaceBookmarksRecyclerviewAdapterBinding
+
+
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = PlaceBookmarksRecyclerviewAdapterBinding.inflate(activity.layoutInflater, parent, false)
+        return ViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindAttributes(position)
+    }
+
+    override fun getItemCount(): Int = listOfPlaceBookmarks.size
+
+
+    inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        fun bindAttributes(position: Int) {
+            val model : PlaceBookmarksRVModel = listOfPlaceBookmarks[position]
+            with(binding){
+                textViewPlaceName.text = model.placeName
+                textViewPlaceText.text = model.placeText
+            }
+        }
+    }
+
+
+
 }
