02011c292fe3d2e56f1668af3ecf51bffb140857
John_Dominic <johndominicjasmin@gmail.com>
2022-01-06 21:21:59 +0800

    Rename classes

--- app/src/main/java/com/example/commutingapp/views/ui/recycler_view_model/WeatherRVModel.kt
+++ app/src/main/java/com/example/commutingapp/views/ui/recycler_view_model/WeatherRVModel.kt
@@ -1,6 +1,6 @@
-package com.example.commutingapp.views.ui.recycler_view
+package com.example.commutingapp.views.ui.recycler_view_model
 
-data class WeatherRecyclerViewModel(
+data class WeatherRVModel(
     val time:String,
     val icon:String? = null,
     val temperature:String,
